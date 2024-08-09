71d40152f8d489cf87627b8bf275930fbf1810e6
Phi Hoang Nhat Ma <mahoangnhatphi@gmail.com>
2024-08-10 00:03:23 +0700

    update ui and video for demo

---
+++ web_app/query/Query20240609.sql
@@ -0,0 +1,14 @@
+Select *
+From videos
+WHERE video_id = 'trom_xe_thuan_an_1.mp4';
+
+Select *
+From DetectTime
+Where video_id = 'trom_xe_thuan_an_1.mp4';
+
+Delete From videos
+Where video_id = 'trom_xe_thuan_an_1.mp4';
+
+Delete From DetectTime
+Where video_id = 'trom_xe_thuan_an_1.mp4';
+
---
+++ web_app/query/Revert_Video_Demo.sql
@@ -0,0 +1,23 @@
+Delete From videos
+Where video_id IN
+('Dan_Canh_Lay_Xe_May.mp4',
+'Lay_Xe_May.mp4',
+'Stealing075_x264_4.mp4',
+'Stealing096_x264.mp4',
+'Stealing112_x264.mp4',
+'Trom_Lien_Tuc_3_Xe_May.mp4',
+'YOUTUBE_YouTubeCCTV031_run_2.mp4',
+'Trom_Xe_May_Ban_Dem.mp4',
+'shudh_l_r.mp4');
+
+Delete From DetectTime
+Where video_id IN
+('Dan_Canh_Lay_Xe_May.mp4',
+'Lay_Xe_May.mp4',
+'Stealing075_x264_4.mp4',
+'Stealing096_x264.mp4',
+'Stealing112_x264.mp4',
+'Trom_Lien_Tuc_3_Xe_May.mp4',
+'YOUTUBE_YouTubeCCTV031_run_2.mp4',
+'Trom_Xe_May_Ban_Dem.mp4',
+'shudh_l_r.mp4')
